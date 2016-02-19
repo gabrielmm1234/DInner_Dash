f7727674567f1945e083d2d343656fb1ebbbcc4e
gabrielmm1234 <gabrielmesquita@cjr.org.br>
2016-02-19 15:13:41 -0200

    Boostrap inicial nas paginas

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160219163102) do
+ActiveRecord::Schema.define(version: 20160219164442) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
