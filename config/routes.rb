55ca87604a982e01b6f1f7e659a774398db49728
gabrielmm1234 <gabrielmesquita@cjr.org.br>
2016-02-19 21:12:27 -0200

    Devise funcionando

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users,:controllers => { registrations: 'users/registrations' }
   resources :items
   resources :categories
   resources :profiles
+  root to: "items#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
