08ba797976a01a707a14de70d17f4e92bc3cd199
gabrielmm1234 <gabrielmesquita@cjr.org.br>
2016-02-22 23:05:53 -0300

    rails lixo do kct

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160219225714) do
+ActiveRecord::Schema.define(version: 20160223004117) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "item_id"
   end
+
+  add_index "categories", ["item_id"], name: "index_categories_on_item_id"
 
   create_table "items", force: :cascade do |t|
     t.string   "Name"
@@ -25,7 +28,18 @@ ActiveRecord::Schema.define(version: 20160219225714) do
     t.float    "Price"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "category_id"
   end
+
+  add_index "items", ["category_id"], name: "index_items_on_category_id"
+
+  create_table "items_categories", id: false, force: :cascade do |t|
+    t.integer "category_id"
+    t.integer "item_id"
+  end
+
+  add_index "items_categories", ["category_id"], name: "index_items_categories_on_category_id"
+  add_index "items_categories", ["item_id"], name: "index_items_categories_on_item_id"
 
   create_table "profiles", force: :cascade do |t|
     t.string   "name"
