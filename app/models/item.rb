ada875b61b68a187e5aed851fd4830ed8a4885f7
gabrielmm1234 <gabrielmesquita@cjr.org.br>
2016-02-24 19:54:13 -0300

    arrumando tretas

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,4 @@
 class Item < ActiveRecord::Base
-<<<<<<< HEAD
 	has_and_belongs_to_many :category
 	validates_presence_of :Name, :Description, :Price
 	accepts_nested_attributes_for :category, :reject_if => lambda { |a| a[:content].blank? }, :allow_destroy => true
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  root to: "items#index"
   devise_for :users,:controllers => { registrations: 'users/registrations' }
   resources :items
   resources :categories
